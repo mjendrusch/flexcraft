90e7be0ea4c602941a8729b4cf821146150f2f7d
Michael Jendrusch <michael.jendrusch@embl.de>
2026-04-23 09:34:32 +0200

    improve collect_success script.

--- flexcraft/pipelines/collect_success.py
+++ flexcraft/pipelines/collect_success.py
@@ -8,7 +8,9 @@ opt = parse_options(
     "Collect successful designs and metrics from a directory of flexcraft runs.",
     in_path="inputs/",
     out_path="outputs/",
-    collect_chains="all"
+    collect_chains="all",
+    dirs_match="none",
+    all_relaxed="False"
 )
 
 os.makedirs(f"{opt.out_path}/designs/", exist_ok=True)
@@ -16,7 +18,14 @@ os.makedirs(f"{opt.out_path}/designs/", exist_ok=True)
 with open(f"{opt.out_path}/success.csv", "wt") as out_f, open(f"{opt.out_path}/success.fa", "wt") as fa_f:
     header_done = False
     for subdir in os.listdir(opt.in_path):
-        path = f"{opt.in_path}/{subdir}/success.csv"
+        if not os.path.isdir(f"{opt.in_path}/{subdir}"):
+            continue
+        if opt.dirs_match != "none" and not subdir.startswith(opt.dirs_match):
+            continue
+        if opt.all_relaxed == "True":
+            path = f"{opt.in_path}/{subdir}/all.csv"
+        else:
+            path = f"{opt.in_path}/{subdir}/success.csv"
         if not os.path.isfile(path):
             continue
         with open(path, "rt") as in_f:
@@ -25,9 +34,20 @@ with open(f"{opt.out_path}/success.csv", "wt") as out_f, open(f"{opt.out_path}/s
             if not header_done:
                 out_f.write(header)
                 header_done = True
-            for line in in_f:
-                out_f.write(subdir + "," + line)
-        success_path = f"{opt.in_path}/{subdir}/success/"
+            if opt.all_relaxed == "True":
+                reason_offset = header.split(",").index("reason") - 1
+                for line in in_f:
+                    reason = line.strip().split(",")[reason_offset]
+                    if "designability" in reason:
+                        continue
+                    out_f.write(subdir + "," + line)
+            else:
+                for line in in_f:
+                    out_f.write(subdir + "," + line)
+        if opt.all_relaxed == "True":
+            success_path = f"{opt.in_path}/{subdir}/relaxed/"
+        else:
+            success_path = f"{opt.in_path}/{subdir}/success/"
         for file_name in os.listdir(success_path):
             design_path = f"{success_path}/{file_name}"
             data = load_pdb(design_path)
